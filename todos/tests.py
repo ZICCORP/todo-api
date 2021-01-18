d6e194b5b883881ea8243dfcc1b07d50a75a2714
ZICCORP <frankchuka250@gmail.com>
2021-01-18 15:11:56 +0100

    Added test for our Todo model

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+class TodoModelTest(TestCase):
+
+    @classmethod
+
+    def setUpTestData(cls):
+        Todo.objects.create(title='First Todo',body='a body here')
+
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEqual(expected_object_name,'First Todo')
+
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.body}'
+        self.assertEqual(expected_object_name,'a body here')
