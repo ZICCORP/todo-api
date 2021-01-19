ba9e36e6b252aab3b01454a7a334d767af10908f
ZICCORP <frankchuka250@gmail.com>
2021-01-19 12:18:30 +0100

    removed the mock data from app.js and used axios to call the todos api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
 
-const list = [
-  {
-    "id": 1,
-    "title": "Learn Javascript",
-    "body": "Familiarize myself with the JavaScript programming language."
-  },
-  {
-    "id": 2,
-    "title": "Learn React",
-    "body": "Familiarize myself  with the JavaScript frontend framework react in order to me able to manipulate the document object model(DOM)"
-  },
-  {
-    "id": 3,
-    "title": "Learn Cascading Style Sheet(CSS)",
-    "body": "Familiarize myself with css in order to be able to beautify my site"
-  }
-]
+import React, { Component } from 'react';
+import axios from 'axios';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { list };
+
+  state = {
+    todos: []
+  };
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    axios
+      .get('http://127.0.0.1:8000/api/')
+      .then(res => {
+        this.setState({ todos: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div>
-        {this.state.list.map(item => (
+        {this.state.todos.map(item => (
           <div key={item.id}>
             <h1>{item.title}</h1>
-            <p>{item.body}</p>
+            <span>{item.body}</span>
           </div>
         ))}
       </div>
